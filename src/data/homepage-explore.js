57690363ebc05916dc17a08ab229da7094d5aa21
rupjit23 <rupjitmaity1@gmail.com>
2023-09-06 20:41:54 +0530

    first commit

---
+++ src/data/homepage-explore.js
@@ -0,0 +1,117 @@
+export const HomePageExplore = [
+    {
+        tag: 'Free',
+        courses : [
+            {
+                heading : "Learn HTML",
+                description : "This course covers the basic concepts of HTML including creating and structuring web pages, adding text, links, images, and more.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Learn CSS",
+                description : "This course breaks down the fundamentals of CSS into digestible, easy to understand pieces. Over the next few modules.",
+                level : 'Beginner',
+                lessionNumber : 7
+            },
+            {
+                heading : "Responsive Web design",
+                description : "This course teaches responsive web design techniques, allowing web pages to adapt to different devices and screen sizes",
+                level : 'Beginner',
+                lessionNumber : 5
+            },
+        ]
+    },
+    {
+        tag: 'New to coding',
+        courses : [
+            {
+                heading : "HTML",
+                description : "HTML is the standard markup language for Web pages with HTML you can create your own Website HTML is easy to learn - You will enjoy it!",
+                level : 'Beginner',
+                lessionNumber : 4
+            },
+            {
+                heading : "CSS",
+                description : "CSS stands for Cascading Style Sheets CSS saves a lot of work,it can control the layout of multiple web pages all at once.",
+                level : 'Beginner',
+                lessionNumber : 8
+            },
+            {
+                heading : "Responsive ",
+                description : "Responsive web design is about creating web pages that look good on all devices! A responsive web design will automatically adjust for different screen sizes and viewports.",
+                level : 'Beginner',
+                lessionNumber : 9
+            },
+        ]
+    },
+    {
+        tag: 'Most popular',
+        courses : [
+            {
+                heading : "Java",
+                description : "Java is a popular programming language Java is used to develop mobile apps, web apps, desktop apps, games and much more.",
+                level : 'Beginner',
+                lessionNumber : 12
+            },
+            {
+                heading : "Python",
+                description : "Python is a popular programming language Python can be used on a server to create web applications",
+                level : 'Beginner',
+                lessionNumber : 8
+            },
+            {
+                heading : "SCSS",
+                description : "Sass is a preprocessor scripting language that is interpreted or compiled into Cascading Style Sheets. SassScript is the scripting language itself.",
+                level : 'Beginner',
+                lessionNumber : 15
+            },
+        ]
+    },
+    {
+        tag: 'Skills paths',
+        courses : [
+            {
+                heading : "Flask",
+                description : "This course covers the basic concepts of HTML including creating and structuring web pages, adding text, links, images, and more.",
+                level : 'Beginner',
+                lessionNumber : 5
+            },
+            {
+                heading : "Django",
+                description : "This course explores advanced topics in HTML5 and CSS3, including animations, transitions, and layout techniques",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Fast API",
+                description : "This course teaches responsive web design techniques, allowing web pages to adapt to different devices and screen sizes",
+                level : 'Beginner',
+                lessionNumber : 7
+            },
+        ]
+    },
+    {
+        tag: 'Career paths',
+        courses : [
+            {
+                heading : "Next.js",
+                description : "This course covers the basic concepts of HTML including creating and structuring web pages, adding text, links, images, and more.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Nuxt.js",
+                description : "This course explores advanced topics in HTML5 and CSS3, including animations, transitions, and layout techniques",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Sanity",
+                description : "This course teaches responsive web design techniques, allowing web pages to adapt to different devices and screen sizes",
+                level : 'Beginner',
+                lessionNumber : 8
+            },
+        ]
+    },
+]
